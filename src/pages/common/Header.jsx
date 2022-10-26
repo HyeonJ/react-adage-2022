222888ae25fd93b7e9342a9dbddf74e2b0a35544
HyeonJ <bpx2710@gmail.com>
2022-10-26 16:20:25 +0900

    merge

--- src/pages/common/Header.jsx
+++ src/pages/common/Header.jsx
@@ -22,8 +22,7 @@ const Header = () => {
                         <ul className="flex-style-2 col-gap-20">
                             {menuArr.map((v, i) => (
                                 <li key={i}>
-                                    <Link className="d-block" to={"/category"+v[1]}>{v[0]}</Link>
-
+                                    <Link className="d-block" to={"/category"+i}>{v[0]}</Link>
                                 </li>
                             ))}
 
